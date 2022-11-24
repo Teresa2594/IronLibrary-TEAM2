dee58c2a40cbdc379d677959dd902eac357f9656
martu <martudorado@gmailcom>
2022-11-24 16:03:04 +0100

    Date done

--- src/main/java/com/ironhack/ironlibrary/Model/Issue.java
+++ src/main/java/com/ironhack/ironlibrary/Model/Issue.java
@@ -1,7 +1,12 @@
 package com.ironhack.ironlibrary.Model;
 
+import org.hibernate.type.LocalDateType;
+
 import javax.persistence.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @Entity
@@ -45,18 +50,19 @@ public class Issue {
     }
 
     public void setIssueDate() {
-        LocalDate date = LocalDate.now();
-        issueDate = date.toString();
-        this.issueDate = issueDate;
+        this.issueDate =  LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
     }
 
     public String getReturnDate() {
         return returnDate;
     }
 
-    public void setReturnDate()
-    {
-        this.returnDate = returnDate;
+    public void setReturnDate()  {
+
+        LocalDate date  = new LocalDateType().fromString(this.issueDate).plusDays(7);
+
+        this.returnDate = date.toString();
     }
 
     public Student getStudent() {
